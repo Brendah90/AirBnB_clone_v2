f4d26cfadb2ec9a8954eb19c51c4d5fc56af7403
EvanieWares <evaniewares@gmail.com>
2024-04-28 05:10:14 +0200

    DBStorage - Place

--- models/city.py
+++ models/city.py
@@ -6,9 +6,10 @@ from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    """The city class, contains state ID and name inheri Base"""
+    """The City class"""
 
     __tablename__ = "cities"
 
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    places = relationship("Place", backref="cities", cascade="all, delete")
